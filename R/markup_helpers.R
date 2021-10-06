6ec9a197b71eb7944ef7623f102083637439c06a
M Schmettow <schmettow@web.de>
2021-10-06 20:07:57 +0200

    + fixed problem with md_coef

--- R/markup_helpers.R
+++ R/markup_helpers.R
@@ -557,51 +557,51 @@ knit_print.tbl_IC_comp <- function(x, ...) {
 #' @author Martin Schmettow
 #' @export
 
-md_coef =
-	function(tbl_coef, ...,
-					 row = NULL,
-					 center = T,
-					 interval = F,
-					 prefix = F,
-					 round = 2,
-					 mean_fnc = identity,
-					 neg = F) {
+md_coef <- function(tbl_coef, ...,
+										row = NULL,
+										center = T,
+										interval = F,
+										prefix = F,
+										round = 2,
+										mean_fnc = identity,
+										neg = F) {
 
-		filter_crit = list(...)
 
-		if(!c("tbl_coef") %in% class(tbl_coef)) stop("coefficient table required as input, e.g. coef(posterior)")
-		if(!is.null(row)){
-			if(!is.numeric(row)) stop("row must be integer")
-			if(row < 1 || row > nrow(tbl_coef)) stop("row must be between ", 1, " and ", nrow(tbl_coef))
-			if(length(filter_crit) > 0) warning("selection by row overides selection by filter")
-			tbl_coef = slice(tbl_coef, row)
-		} else {
-			tbl_coef = filter(tbl_coef, filter_crit)
-		}
-
-		if(nrow(tbl_coef) == 0) stop("md_coef: parameter does not exist")
-		if(nrow(tbl_coef) > 1)  stop("md_coef: parameter is not unique, ", print(tbl_coef))
-
-		out = as.character()
-
-		if(prefix) warning("prefix not yet implemented")
-
-		if(neg) {
-			tbl_coef$center <- -tbl_coef$center
-			tbl_coef$lower <- -tbl_coef$lower
-			tbl_coef$upper <- -tbl_coef$upper
-		}
-
-		if(center) out =
-			stringr::str_c(out, round(mean_fnc(tbl_coef[[1, "center"]]), round))
-
-		if(interval) out =
-			stringr::str_c(out,
-										 " [", round(mean_fnc(tbl_coef[[1, "lower"]]), round), ", ",
-										 round(mean_fnc(tbl_coef[[1, "upper"]]), round),
-										 "]_{CI",attr(tbl_coef, "interval") * 100 ,"}")
-		out
+	if(0 == sum(stringr::str_detect(class(tbl_coef),
+																	"tbl_clu|tbl_coef")))
+		stop("coefficient table required as input, e.g. coef(posterior)")
+	if(!is.null(row)){
+		if(!is.numeric(row)) stop("row must be integer")
+		if(row < 1 || row > nrow(tbl_coef)) stop("row must be between ", 1, " and ", nrow(tbl_coef))
+		#if(length(...) > 0) warning("selection by row overides selection by filter")
+		tbl_coef = slice(tbl_coef, row)
+	} else {
+		tbl_coef = filter(tbl_coef, ...)
 	}
+
+	if(nrow(tbl_coef) == 0) stop("md_coef: parameter does not exist")
+	if(nrow(tbl_coef) > 1)  stop("md_coef: parameter is not unique, ", print(tbl_coef))
+
+	out = as.character()
+
+	if(prefix) warning("prefix not yet implemented")
+
+	if(neg) {
+		tbl_coef$center <- -tbl_coef$center
+		tbl_coef$lower <- -tbl_coef$lower
+		tbl_coef$upper <- -tbl_coef$upper
+	}
+
+	if(center) out =
+		stringr::str_c(out, round(mean_fnc(tbl_coef[[1, "center"]]), round))
+
+	if(interval) out =
+		stringr::str_c(out,
+									 " [", round(mean_fnc(tbl_coef[[1, "lower"]]), round), ", ",
+									 round(mean_fnc(tbl_coef[[1, "upper"]]), round),
+									 "]_{CI",attr(tbl_coef, "interval") * 100 ,"}")
+	out
+}
 
 #' @rdname md_coef
 #' @export
@@ -614,7 +614,7 @@ frm_coef =
 					 prefix = F,
 					 round = 2,
 					 neg = F) {
-		out = bayr::md_coef(tbl_coef, ...,
+		out =  md_coef(tbl_coef, ...,
 												row = row,
 												center = center,
 												interval = interval,
